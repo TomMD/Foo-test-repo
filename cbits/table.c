5352c01f7af480dc39870b541200f13d9d58895e
Thomas M. DuBuisson <tommd@galois.com>
2014-05-14 15:18:26 -0700

    Remove comment that makes no sense in this dummy context.

--- cbits/table.c
+++ cbits/table.c
@@ -3,11 +3,6 @@
 
 typedef uint8_t row_t[256] __attribute__((aligned(16)));
 
-/* row is a pointer to a 256-byte vector
- * table is a pointer to 64 256-byte vectors
- * n is a 256-bit vector
- * Compute: row += n . table
- */
 void rowSum(row_t row, row_t *table, uint64_t n) {
         int j;
         for (; n; n>>=1, table++) {
